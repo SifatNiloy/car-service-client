684f52df3455760706ec7c388c4f940fffeaeda8
SifatNiloy <sifatniloy18@gmail.com>
2023-02-09 20:43:52 +0600

    banner first slider making

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -10,8 +10,21 @@ const Banner = () => {
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full" alt='' />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className="text-6xl font-bold text-white" >
+                        Affordable <br />
+                        Price For Car<br />
+                        Servicing
+                    </h1>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                    <p className="text-xl font-bold text-white" >
+                        <button className="btn btn-warning mr-5">Warning</button>
+                        <button className="btn btn-outline btn-warning">Warning</button>
+                    </p>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn mr-5 btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
