46ecd4169544c1e942f05eccf0760a638c13a527
SifatNiloy <sifatniloy18@gmail.com>
2023-02-12 18:53:27 +0600

    about component created

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import img1 from '../../assets/images/banner/1.jpg'
-import img2 from '../../assets/images/banner/2.jpg'
-import img3 from '../../assets/images/banner/3.jpg'
-import img4 from '../../assets/images/banner/4.jpg'
-import img5 from '../../assets/images/banner/5.jpg'
-import img6 from '../../assets/images/banner/6.jpg'
-import './Banner.css';
+import img1 from '../../../assets/images/banner/1.jpg'
+import img2 from '../../../assets/images/banner/2.jpg'
+import img3 from '../../../assets/images/banner/3.jpg'
+import img4 from '../../../assets/images/banner/4.jpg'
+import img5 from '../../../assets/images/banner/5.jpg'
+import img6 from '../../../assets/images/banner/6.jpg'
+
 import BannerItem from './BannerItem';
 
 const bannerData = [
@@ -49,7 +49,7 @@ const bannerData = [
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full py-10">
 
             {
                 bannerData.map(slide => <BannerItem
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,4 +1,5 @@
-import Home from "../../Pages/Home/Home";
+import Home from "../../Pages/Home/Home/Home";
+
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../../Layout/Main");
