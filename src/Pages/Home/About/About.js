ac44201a3bbc1d7fdb92b933a786c2c494b9a757
SifatNiloy <sifatniloy18@gmail.com>
2023-02-13 11:51:53 +0600

    services component added

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -3,7 +3,7 @@ import person from '../../../assets/images/about_us/person.jpg'
 import parts from '../../../assets/images/about_us/parts.jpg'
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
                 <div className='w-1/2 relative'>
                     <img src={person} className='w-4/5 rounded-lg shadow-2xl' alt='' />
@@ -11,7 +11,7 @@ const About = () => {
                 </div>
                 <div className='w-1/2'>
                     <p className='text-2xl font-bold text-orange-600'>About Us</p>
-                    <h1 className="text-5xl font-bold">
+                    <h1 className="text-5xl my-5 font-bold">
                         We are qualified <br />
                         & of experience in this field</h1>
                     <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import arrow from '../../../assets/icons/next.png'
+const ServiceCard = ({service}) => {
+    const {title, img, price, description}= service;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <div className='flex'>
+                    <p className='text-red-600 font-semibold text-xl'>Price : ${price}</p>
+                    <img style={{ width: "20px", color:"red" }} src={arrow} alt="" />
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
