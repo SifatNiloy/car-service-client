2f7e038416cff15fa90c83b35206f9726d9c96eb
SifatNiloy <sifatniloy18@gmail.com>
2023-02-14 13:49:11 +0600

    firebase config added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB856kydlv4UjOqI1bQdLJtN7h07iT0Z_Y",
+    authDomain: "car-service-e00e3.firebaseapp.com",
+    projectId: "car-service-e00e3",
+    storageBucket: "car-service-e00e3.appspot.com",
+    messagingSenderId: "1020069725787",
+    appId: "1:1020069725787:web:be0a3cbc8246384a5da17e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
