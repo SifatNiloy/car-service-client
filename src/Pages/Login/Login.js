cd807eab01ba6b0243be4eef4471d06209ad3c3e
SifatNiloy <sifatniloy18@gmail.com>
2023-02-14 13:39:18 +0600

    login form created

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import login from '../../assets/images/login/login.svg'
 const Login = () => {
+    const handleSubmit=event=>{
+        event.preventDefault();
+    }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="hero w-full py-20">
+            <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row ">
+                <div className="text-center lg:text-left pt-32">
+                    <img className='w-3/4' src={login} alt="" />
+                   
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <h1 className="text-5xl text-center font-bold">Login </h1>
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -22,13 +26,13 @@ const Login = () => {
                             </label>
                             <input type="text" placeholder="password" className="input input-bordered" />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <a href="/login" className="label-text-alt link link-hover" >Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <input className='btn btn-primary' type="submit" value="Login" />
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
