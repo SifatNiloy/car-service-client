9ab10b7dec69e4878552798a3b3a8a3f2f31cccd
SifatNiloy <sifatniloy18@gmail.com>
2023-02-22 12:27:11 +0600

    login authentication added

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../../firebase/firebase.config';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth'
 export const AuthContext = createContext();
 const auth= getAuth(app);
 
@@ -11,12 +11,12 @@ const AuthProvider = ({children}) => {
     const createUser=(email ,password)=>{
         return createUserWithEmailAndPassword(auth, email, password)
     }
-    const authInfo={
-        user, 
-        loading,
-        createUser
+  
 
+    const login=(email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password);
     }
+
     useEffect(()=>{
         const unsubscribe= onAuthStateChanged(auth, currentUser=>{
             console.log(currentUser);
@@ -26,6 +26,15 @@ const AuthProvider = ({children}) => {
             return unsubscribe();
         }
     } , [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        login
+
+    }
+    
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
